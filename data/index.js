68082d9c1544ba59cbeecf1164da77763fc021da
dbvd28 <60490932+dbvd28@users.noreply.github.com>
2025-04-07 16:01:13 -0600

    feat:se trata de solucionar referencias en el deploy

--- data/index.js
+++ data/index.js
@@ -1,25 +1,25 @@
 const base = process.env.DEPLOY_BASE_URL || '/';
-
+if (!base.endsWith('/')) base += '/';
 
 const commonData = {
   navigation: [
-    { url: `${base}/index.html`, label: 'Inicio' },
-    { url: `${base}/faq.html`, label: 'Faq' },
-    { url: `${base}/Eventos.html`, label: 'Eventos' },
-    { url: `${base}/aboutus.html`, label: 'Acerca de nosotros' }
+    { url: `${base}index.html`, label: 'Inicio' },
+    { url: `${base}faq.html`, label: 'Faq' },
+    { url: `${base}Eventos.html`, label: 'Eventos' },
+    { url: `${base}aboutus.html`, label: 'Acerca de nosotros' }
   ]
 };
 
 export const getPageContext = (pagePath) => {
   let pageData = {};
   switch (pagePath) {
-    case '/index.html':
+    case 'index.html':
       break;
-    case '/Eventos.html':
+    case 'Eventos.html':
       break;
-    case '/faq.html':
+    case 'faq.html':
       break;
-    case '/aboutus.html':
+    case 'aboutus.html':
       break;
     default:
       break;
