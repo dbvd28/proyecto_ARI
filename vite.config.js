c3544f9351b642583547f779784a0c465e053ad0
dbvd28 <60490932+dbvd28@users.noreply.github.com>
2025-04-07 15:22:48 -0600

    feat:se trata de solucionar referencias en el deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import path, { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import { globSync } from 'glob';
+import * as glob from 'glob'
 import htmlPurge from 'vite-plugin-purgecss';
 import handlebars from 'vite-plugin-handlebars';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
